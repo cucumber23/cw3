bec27ff9baa31608e86698cfdedd3e2247540ada
Ilgiz <khamilgiz@gmail.com>
2023-03-10 01:28:51 +0500

    add main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils import get_data, get_filtered_data, get_last_data, get_formatted_data
 
 def main():
-    OPERATIONS_URL = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678359787437&signature=MNJ80F8anAPw9-qsVcAaM3JxeNn0JRYJivitXIkBNlg&downloadName=operations.json"
+    OPERATIONS_URL = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678471809323&signature=ADThtSftYPnFplSsz6u_s6YPC2tAYVH8pX-yqwbnYlA&downloadName=operations.json"
     COUNT_LAST_VALUES = 5
     FILTERED_EMPTY_FROM = True
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,13 +4,13 @@ from utils import get_data, get_last_data, get_formatted_data, get_filtered_data
 
 
 def test_get_data():
-    url = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678359787437&signature=MNJ80F8anAPw9-qsVcAaM3JxeNn0JRYJivitXIkBNlg&downloadName=operations.json"
+    url = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678471809323&signature=ADThtSftYPnFplSsz6u_s6YPC2tAYVH8pX-yqwbnYlA&downloadName=operations.json"
     assert get_data(url) is not None
-    url = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678359787437&signature=MNJ80F8anAPw9-qsVcAaM3JxeNn0JRYJivitXIkBNlg&downloadNam=operations.json"
+    url = "https://file.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678471809323&signature=ADThtSftYPnFplSsz6u_s6YPC2tAYVH8pX-yqwbnYlA&downloadNam=operations.json"
     data, info = get_data(url)
     assert data is None
     assert info == "WARNING: Статус ответа 400"
-    url = "https://fil.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678359787437&signature=MNJ80F8anAPw9-qsVcAaM3JxeNn0JRYJivitXIkBNlg&downloadName=operations.json"
+    url = "https://fil.notion.so/f/s/d22c7143-d55e-4f1d-aa98-e9b15e5e5efc/operations.json?spaceId=0771f0bb-b4cb-4a14-bc05-94cbd33fc70d&table=block&id=f11058ed-10ad-42ea-a13d-aad1945e5421&expirationTimestamp=1678471809323&signature=ADThtSftYPnFplSsz6u_s6YPC2tAYVH8pX-yqwbnYlA&downloadName=operations.json"
     data, info = get_data(url)
     assert data is None
     assert info == "ERROR: requests.exceptions.ConnectionError"
